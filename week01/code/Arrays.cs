87549d470d53f13703be30ec2b86ee8bfc4f0c59
Zaanian <sstauffer1@byupathway.edu>
2026-01-08 10:49:00 -0800

    Rotate List Right function success

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -13,7 +13,18 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        // 
+        var result = new double[length];
+        //
+        var y = 1;
+        for (var i = 0; i < length; i++)
+        {
+            result[i] = y * number;
+            y++;
+        }
+
+
+        return result; // replace this return statement with your own
     }
 
     /// <summary>
@@ -29,5 +40,16 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+        var LstIdx = 0;
+        var x = 0;
+        for (var i = 0; i < amount; i++)
+        {
+            LstIdx = data.Count - 1;
+            x = data[LstIdx];
+            data.RemoveAt(LstIdx);
+            data.Insert(0, x);
+
+        }
+
     }
 }
